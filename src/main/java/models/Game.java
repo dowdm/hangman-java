40820d739c07742f5e555e0664e9a8a0249e315a
Matt Dowdney and semhar kidane <semhar@>
2018-05-23 11:15:38 -0700

    add test for loss condition, add set method for eliminatin random test variables

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -1,4 +1,44 @@
 package models;
 
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Game {
+    private String chosenWord = " ";
+    private String [] wordBank =  {"apples", "bananas", "oranges", "pears"};
+    private Random randomWordChooser = new Random();
+    private List<Character> unusedLetters = new ArrayList<Character>();
+    private List<Character> usedLetters = new ArrayList<Character>();
+
+    public String generateWord(){
+        int getPosition = randomWordChooser.nextInt(4);
+        return wordBank[getPosition];
+    }
+
+    public void setWordChoice(String wordChoice){
+       this.chosenWord = wordChoice;
+    }
+
+    public boolean letterCheck(char letterGuess){
+        if (chosenWord.indexOf(letterGuess) == -1){
+            unusedLetters.add(letterGuess);
+            return false;
+        } else{
+            usedLetters.add(letterGuess);
+            return true;
+        }
+    }
+
+    public boolean lossCheck(){
+        if(unusedLetters.size() >= 6){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
+
--- src/test/java/models/GameTest.java
+++ src/test/java/models/GameTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.*;
 
 public class GameTest {
@@ -10,5 +11,31 @@ public class GameTest {
     public void newGame_instatiatesCorrectly() throws Exception {
         Game testGame = new Game ();
         assertEquals(true, testGame instanceof Game);
+    }
+    @Test
+    public void returnWord_checksForRandomWordChoice_true() throws Exception {
+        Game testGame = new Game ();
+        boolean expected = true;
+        assertEquals(true, testGame.generateWord() instanceof String);
+    }
+    @Test
+    public void letterCheck_checksIfCharacterIsInWord_true() throws Exception {
+        Game testGame = new Game ();
+        testGame.setWordChoice("apple");
+        boolean expected = true;
+        assertEquals(true, testGame.letterCheck('a') );
+    }
+    @Test
+    public void lossCheck_checksIfPlayerLoses_true() throws Exception {
+        Game testGame = new Game ();
+        testGame.setWordChoice("apple");
+        testGame.letterCheck('d');
+        testGame.letterCheck('f');
+        testGame.letterCheck('g');
+        testGame.letterCheck('m');
+        testGame.letterCheck('h');
+        testGame.letterCheck('i');
+        boolean expected = true;
+        assertEquals(true, testGame.lossCheck());
     }
 }
