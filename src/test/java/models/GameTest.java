9d54a0a4b1d9cfb33c346934fc4aebf1fa564e32
Matt Dowdney and semhar kidane <semhar@>
2018-05-23 08:15:31 -0700

    adds first test for testing instantiation of Game

--- src/test/java/models/GameTest.java
+++ src/test/java/models/GameTest.java
@@ -1,7 +1,14 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class GameTest {
 
+    @Test
+    public void newGame_instatiatesCorrectly() throws Exception {
+        Game testGame = new Game ();
+        assertEquals(true, testGame instanceof Game);
+    }
 }
